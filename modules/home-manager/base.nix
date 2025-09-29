61cc3b7b9f074135f9f79b6de00cbc2c91bcfeb8
Jacopone <jacopo.anselmi@gmail.com>
2025-09-29 21:12:59 +0200

    fixed broot initialization

--- modules/home-manager/base.nix
+++ modules/home-manager/base.nix
@@ -336,6 +336,19 @@
       # Development shortcuts
       alias bench='hyperfine'
       alias loc='tokei'
+
+      # Broot integration function (enables cd functionality)
+      function br --wraps=broot
+          set -l cmd_file (mktemp)
+          if broot --outcmd $cmd_file $argv
+              source $cmd_file
+              rm -f $cmd_file
+          else
+              set -l code $status
+              rm -f $cmd_file
+              return $code
+          end
+      end
       
       # New tool aliases
       alias lintpy='ruff check'
